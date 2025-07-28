6a8e6dbc97daad73622441cfacab15b5d13494ac
Joey Blackwell <blackwellj6241@gmail.com>
2025-07-28 13:07:15 -0500

    added habbit list function

---
+++ src/habitList.jsx
@@ -0,0 +1,15 @@
+export const habitList = ({habits, onToggle}) => {
+    return(
+        <ul>
+            {habits.map(habit)=>(
+                <li key={habit.id}>
+                    <span>{habit.name}</span>
+                    <button onClick={()=>onToggle(habit.id)}>
+                        {habit.completedToday ? "☑️" : "✖️"}
+                    </button>
+
+                </li>
+            )}
+        </ul>
+    )
+}
