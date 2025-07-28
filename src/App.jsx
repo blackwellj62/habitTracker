3347088c63eb13aa67546754d8e6d1f9bbff2d05
Joey Blackwell <blackwellj6241@gmail.com>
2025-07-28 12:54:31 -0500

    added habit placeholders

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,15 @@ import { useState } from 'react'
 import './App.css'
 
 function App() {
-  
+  const [habits, setHabits] = useState([
+    {id: 1, name:"morning stretches", completedToday: false},
+    {id: 2, name: "exercise", completedToday:false},
+    {id:3, name: "code", completedToday:false}
+  ])
 
   return (
     <>
-      
+      <h1>Habit Tracker</h1>
     </>
   )
 }
